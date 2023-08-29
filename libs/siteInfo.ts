a59edc1226a5ce5b79ec08724f5143838d4146bf
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2023-08-30 06:05:18 +0900

    style

--- libs/siteInfo.ts
+++ libs/siteInfo.ts
@@ -3,5 +3,6 @@ export const siteName = "Satoha's Blog";
 export const PER_PAGE = 5;
 
 // https://panda-css.com/docs/guides/dynamic-styling#referenced-values
+
 // 同じファイル内で定義しないとダメ
 // export const siteMaxWidth = "1280px";
