8d3ec5a93e13b927a97b9d6fe3552bed1b0697c6
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2023-09-12 01:41:38 +0900

    style 型の当て方

--- src/app/_components/BlogTags.tsx
+++ src/app/_components/BlogTags.tsx
@@ -1,11 +1,11 @@
 import { HStack } from "../../../styled-system/jsx";
 import { tagStyle } from "../styles/style";
 
-type BlogTagsType = {
-	tags: Array<string>;
+type Props = {
+	tags: string[];
 };
 
-export const BlogTags = ({ tags }: BlogTagsType) => {
+export const BlogTags = ({ tags }: Props) => {
 	return (
 		<HStack mt="3" mb="3">
 			{tags.map((tag, index) => {
