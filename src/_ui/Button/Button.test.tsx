2a697de8e422313ef9beb3d496cc17c404176394
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2023-12-03 03:23:18 +0900

    add: test

--- src/_ui/Button/Button.test.tsx
+++ src/_ui/Button/Button.test.tsx
@@ -1,17 +1,20 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Button from "./Button";
 
-describe("ButtonのLabel表示", () => {
-	test("solid style", () => {
-		render(<Button visual="solid">taro</Button>);
-		expect(screen.getByText("taro")).toBeInTheDocument();
+describe("Buttonコンポーネント", () => {
+	test("子要素の正しい表示", () => {
+		render(<Button>Child Content</Button>);
+		expect(screen.getByText("Child Content")).toBeInTheDocument();
 	});
-	test("outline style", () => {
-		render(<Button visual="outline">Tech</Button>);
-		expect(screen.getByText("Tech")).toBeInTheDocument();
+
+	test("クリックイベントの動作", () => {
+		const handleClick = jest.fn();
+		render(<Button onClick={handleClick}>Click Me</Button>);
+		fireEvent.click(screen.getByText("Click Me"));
+		expect(handleClick).toHaveBeenCalledTimes(1);
 	});
-	test("menu style", () => {
-		render(<Button visual="menu">Tech</Button>);
-		expect(screen.getByText("Tech")).toBeInTheDocument();
+	test("サイズプロパティ（md）の適用", () => {
+		const { container } = render(<Button size="md">Md Button</Button>);
+		expect(container.firstChild).toHaveClass("button--size_md"); // pandaのスタイル
 	});
 });
