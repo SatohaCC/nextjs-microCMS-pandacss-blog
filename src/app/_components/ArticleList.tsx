c7a7216e06820ebd7dcf5f2d29de49ebccb88125
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2023-12-02 20:04:04 +0900

    del console.log

--- src/app/_components/ArticleList.tsx
+++ src/app/_components/ArticleList.tsx
@@ -10,7 +10,7 @@ const ArticleList = async ({ contents }: Props) => {
 	if (!contents || contents.length === 0) {
 		return <h1>No contents</h1>;
 	}
-	console.log(contents[0].categories);
+	
 
 	return (
 		<div>
