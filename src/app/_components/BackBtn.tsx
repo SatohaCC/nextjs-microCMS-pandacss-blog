0e53267d3478e41a7914b62a14dce1808e883143
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2023-12-02 16:17:20 +0900

    style

--- src/app/_components/BackBtn.tsx
+++ src/app/_components/BackBtn.tsx
@@ -1,18 +1,11 @@
 "use client";
 
+import Button from "@/_ui/Button/Button";
 import { useRouter } from "next/navigation";
-import { button } from "../styles/style";
 
 const BackBtn = () => {
 	const router = useRouter();
-	return (
-		<button
-			className={button({ visual: "solid", size: "sm" })}
-			onClick={() => router.back()}
-		>
-			戻る
-		</button>
-	);
+	return <Button onClick={() => router.back()}>戻る</Button>;
 };
 
 export default BackBtn;
