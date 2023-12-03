55eb408f82479f63bece1781888dae8d430d2f43
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2023-12-03 16:53:14 +0900

    rename: nameStyle->style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,16 +3,16 @@ import { css } from "../../styled-system/css";
 import { Box } from "../../styled-system/jsx";
 import Button from "../_ui/Button/Button";
 
-const nameStyle = css({
+const style = css({
 	color: "accent.default",
 });
 
 const NotFound = () => {
 	return (
 		<Box textAlign="center" py={10} px={6}>
-			<h1 className={nameStyle}>404</h1>
-			<h2 className={nameStyle}>Page Not Found</h2>
-			<p className={nameStyle}>
+			<h1 className={style}>404</h1>
+			<h2 className={style}>Page Not Found</h2>
+			<p className={style}>
 				The page you&apos;re looking for does not seem to exist
 			</p>
 			<div className={css({ padding: "4" })} />
