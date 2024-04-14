7a32419fa7a5ff84f07f38204ab21ede5e198646
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2024-04-14 21:28:26 +0900

    style

--- src/_ui/Pagination/Pagination.tsx
+++ src/_ui/Pagination/Pagination.tsx
@@ -1,39 +1,47 @@
-import {Center, HStack} from "../../../styled-system/jsx";
-import {PaginationButton} from "./PagenationButton";
+import { Center, HStack } from '../../../styled-system/jsx';
+import { PaginationButton } from './PagenationButton';
 
 type Props = {
-	topPage: number;
-	lastPage: number;
-	pageList: number[];
-	currentPage: number;
-	category: string;
+    topPage: number;
+    lastPage: number;
+    pageList: number[];
+    currentPage: number;
+    category: string;
 };
 
-  const Pagination = ({ topPage, lastPage, pageList, currentPage ,category}: Props) => {
-
+const Pagination = ({ topPage, lastPage, pageList, currentPage, category }: Props) => {
     if (lastPage === 1) {
         return (
-          <Center p={5}>
-            <HStack>
-              <PaginationButton page={topPage} currentPage={currentPage} category={category} />
-            </HStack>
-          </Center>
+            <Center p={5}>
+                <HStack>
+                    <PaginationButton
+                        page={topPage}
+                        currentPage={currentPage}
+                        category={category}
+                    />
+                </HStack>
+            </Center>
         );
-      }
+    }
 
-	return (
-	  <Center p={5}>
-		<HStack>
-		  <PaginationButton page={topPage} currentPage={currentPage} category={category} />
-		  {currentPage > 3 && <div>・・・</div>}
-		  {pageList.map((page, index) => (
-			  <PaginationButton key={index} page={page} currentPage={currentPage} category={category} />
-		  ))}
-		  {currentPage < lastPage - 2 && <div>・・・</div>}
-		  <PaginationButton page={lastPage} currentPage={currentPage} category={category} />
-		</HStack>
-	  </Center>
-	);
-  };
+    return (
+        <Center p={5}>
+            <HStack>
+                <PaginationButton page={topPage} currentPage={currentPage} category={category} />
+                {currentPage > 3 && <div>・・・</div>}
+                {pageList.map((page, index) => (
+                    <PaginationButton
+                        key={index}
+                        page={page}
+                        currentPage={currentPage}
+                        category={category}
+                    />
+                ))}
+                {currentPage < lastPage - 2 && <div>・・・</div>}
+                <PaginationButton page={lastPage} currentPage={currentPage} category={category} />
+            </HStack>
+        </Center>
+    );
+};
 
 export default Pagination;
